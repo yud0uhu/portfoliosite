3aca19b6c98de932387a2aa5c5bb99e4cff90795
osaka-yu <osaka-yu@dmm.com>
2024-01-29 02:39:14 +0900

    :sparkles: lottie animationの追加と実装

---
+++ src/components/MelonSodaAnimation.svelte
@@ -0,0 +1,14 @@
+
+<script>
+    import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
+</script>
+  
+<LottiePlayer
+src="https://lottie.host/937512b8-72f3-4060-87e1-85e2704a61e3/8auIurXqjI.json"
+autoplay="{true}"
+loop="{true}"
+renderer="svg"
+background="transparent"
+height="{600}"
+width="{600}"
+/>
