88719e2fd3c45e9a3afe673eba6bb951985c4f6b
osaka-yu <osaka-yu@dmm.com>
2024-01-29 03:03:02 +0900

    :ticket: メニュー横幅のmarginを修正

--- src/components/MelonSodaAnimation.svelte
+++ src/components/MelonSodaAnimation.svelte
@@ -6,7 +6,7 @@
 <LottiePlayer
 src="https://lottie.host/937512b8-72f3-4060-87e1-85e2704a61e3/8auIurXqjI.json"
 autoplay="{true}"
-loop="{true}"
+loop="{false}"
 renderer="svg"
 background="transparent"
 height="{600}"
