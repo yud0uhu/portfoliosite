5b9260752911201661634daf7deb9193c1f61ed7
osaka-yu <osaka-yu@dmm.com>
2024-01-28 23:20:56 +0900

    :sparkles: svelte,tailwindのセットアップ

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import cloudflare from "@astrojs/cloudflare";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  })],
+  integrations: [
+    svelte(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
