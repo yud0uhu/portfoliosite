34c102c99c20bdc277940b72c2bf7bb2bc83f5b4
yud0uhu <cist.b219.y.osaka@gmail.com>
2024-08-17 05:58:35 +0900

    :green_heart: fix fmt

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -3,14 +3,10 @@ import {
   type MicroCMSQueries,
   type MicroCMSImage,
 } from "microcms-js-sdk";
-// import { MICROCMS_SERVICE_DOMAIN, MICROCMS_API_KEY } from "$env/static/private";
-
-const MICROCMS_SERVICE_DOMAIN = "https://flyingcat.microcms.io/api/v1/";
-const MICROCMS_API_KEY = "3fcabaf9-e211-4f84-aa89-c627cac3d947";
 
 const client = createClient({
-  serviceDomain: MICROCMS_SERVICE_DOMAIN,
-  apiKey: MICROCMS_API_KEY,
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
 });
 
 export type Works = {
@@ -19,7 +15,7 @@ export type Works = {
   githubUrl: string;
   productDatail: string;
   productComposition: string;
-  productImage: string;
+  productImage: MicroCMSImage;
   productUrl: string;
 };
 export type WorksResponse = {
@@ -32,9 +28,10 @@ export type WorksResponse = {
 export const getList = async (queries?: MicroCMSQueries) => {
   return await client.get<WorksResponse>({ endpoint: "works", queries });
 };
+
 export const getDetail = async (
   contentId: string,
-  queries?: MicroCMSQueries
+  queries?: MicroCMSQueries,
 ) => {
   return await client.getListDetail<Works>({
     endpoint: "works",
