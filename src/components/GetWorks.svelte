6aae06c84f3e8ff7d9e6a1aae5fbbecf0beb93a0
yud0uhu <cist.b219.y.osaka@gmail.com>
2024-08-17 14:18:07 +0900

    :bug: spのときworksの画面幅が崩れるため修正

--- src/components/GetWorks.svelte
+++ src/components/GetWorks.svelte
@@ -14,19 +14,19 @@
 </script>
 
 <div class={cn(["flex", "flex-col", "h-full", "bg-yellow-50"])}>
-  <div class={cn(["p-4", "grid", "grid-cols-3", "gap-4", "justify-center", "items-center", "content-div"])}>
+  <div class={cn(["p-4", "grid", "grid-cols-1", "md:grid-cols-3", "gap-4", "justify-center", "items-center", "content-div"])}>
     {#each response.contents as content, index}
-      <div class={cn(["w-full", "h-full", "p-4", "border", "border-[#ff4f4b]", "flex-shrink-0", index >= 3 && !$showAllContent ? "hidden" : ""])}>
+      <div class={cn(["w-full", "h-auto", "p-4", "border", "border-[#ff4f4b]", "flex-shrink-0", index >= 3 && !$showAllContent ? "hidden" : ""])}>
         <h2 class={cn(["text-xl", "font-semibold", "text-gray-800"])}>{content.productTitle}</h2>
-        <p class={cn(["text-sm", "text-gray-600"])}>{content.productData}</p>
+        <p class={cn(["text-sm", "text-gray-600", "break-words"])}>{content.productData}</p>
         <a href={content.githubUrl} class={cn(["text-teal-500", "hover:underline"])}>GitHub</a>
-        <p class={cn(["text-sm", "text-gray-600"])}>{content.productDatail}</p>
-        <p class={cn(["text-sm", "text-emerald-300"])}>{content.productComposition}</p>
+        <p class={cn(["text-sm", "text-gray-600", "break-words"])}>{content.productDatail}</p>
+        <p class={cn(["text-sm", "text-emerald-300", "break-words"])}>{content.productComposition}</p>
         <a href={content.productUrl} class={cn(["text-teal-500", "hover:underline"])}>URL</a>
       </div>
     {/each}
   </div>
-  <div class={cn(["flex", "justify-center", "items-center", "w-screen"])}>
+  <div class={cn(["flex", "justify-center", "items-center", "w-full"])}>
     <button on:click={handleClick} class={cn(["mt-4", "px-4", "py-2", "bg-teal-500", "text-white", "rounded", "hover:bg-teal-600", "more-button"])}>
       もっと見る
     </button>
