5f94aefb2708b7491f2711c1c570d18b9461be6e
osaka-yu <osaka-yu@dmm.com>
2024-12-29 22:06:23 +0900

    :sparkles: アニメーション読み込み時のパフォーマン改善のためMelonSodaAnimationコンポーネントをDotLottieSvelteに修正し、ナビゲーションスタイルのレスポンシブ制御をtailwindに統一

--- src/components/MelonSodaAnimation.svelte
+++ src/components/MelonSodaAnimation.svelte
@@ -1,13 +1,9 @@
-<script>
-  import { LottiePlayer } from '@lottiefiles/svelte-lottie-player'
-</script>
-
-<LottiePlayer
-  src="https://lottie.host/937512b8-72f3-4060-87e1-85e2704a61e3/8auIurXqjI.json"
-  autoplay={true}
-  loop={false}
-  renderer="svg"
-  background="transparent"
-  height="100%"
-  width="100%"
-/>
+<script lang="ts">
+  import { DotLottieSvelte } from '@lottiefiles/dotlottie-svelte';
+  </script>
+  
+  <DotLottieSvelte
+    src="/images/melon-soda.lottie"
+    loop
+    autoplay
+  />
