ba4eefd330a7c0dd36c7b28b9008c813a838b231
yud0uhu <cist.b219.y.osaka@gmail.com>
2025-02-28 05:50:26 +0900

    :sparkles: FCP/LCP改善のため、Daily UIコンポーネントをLazy Loadingするように修正

--- src/components/GetDailyUI.svelte
+++ src/components/GetDailyUI.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import { cn } from "../lib/utils.ts";
   import { writable } from "svelte/store";
+  import { onMount } from 'svelte';
   export let dailyUIResponse;
   const showAllContent = writable(false);
+  import { fade } from 'svelte/transition';
 
+  let divs = dailyUIResponse.contents.map(() => null);
+  let scrolls = writable(dailyUIResponse.contents.map(() => false));
+  console.log(dailyUIResponse.contents)
   function handleClick() {
     showAllContent.set(true);
     const moreButton = document.querySelector(".daily-ui-more-button");
@@ -11,19 +16,41 @@
       moreButton.classList.add("hidden");
     }
   }
+
+  onMount(() => {
+    let callback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          scrolls[entry.target.dataset.index] = true;
+        }
+      });
+    };
+
+    let observer = new IntersectionObserver(callback, { threshold: 0.1 });
+
+    divs.forEach((div, index) => {
+      console.log(divs);
+      if (div) {
+        div.dataset.index = index;
+        observer.observe(div);
+      }
+    });
+  });
 </script>
 
-<div class={cn(["flex", "flex-col", "p-4", "h-fit", "bg-yellow-50"])}>
+<div id="container" class={cn(["flex", "flex-col", "p-4", "h-fit", "bg-yellow-50"])}>
   <div class={cn(["p-4", "grid", "grid-cols-1", "md:grid-cols-3", "gap-4", "justify-center", "items-center", "content-div"])}>
     {#each dailyUIResponse.contents as content, index}
-      <div class={cn(["w-full", "p-4", "border", "border-[#ff4f4b]", "flex-shrink-0", index >= 3 && !$showAllContent ? "hidden" : ""])}>
-        <h2 class={cn(["text-xl", "font-semibold", "text-gray-800"])}>{content.title}</h2>
-        
-        <img src={content.image.url} width={content.image.width} alt={content.title} class={cn(["object-cover","border-[#ff4f4b]", "p-2"])} />
-        {#if content.figma}
-        <a href={content.figma} class={cn(["text-teal-500", "hover:underline"])}>Figmaを見る</a>
-        {/if}
-      </div>
+      
+        <div bind:this={divs[index]} transition:fade={{ duration: 300 }} class={cn(["w-full", "p-4", "border", "border-[#ff4f4b]", "flex-shrink-0", index >= 3 && !$showAllContent ? "hidden" : ""])}>
+          {#if scrolls[index]}
+            <h2 class={cn(["text-xl", "font-semibold", "text-gray-800"])}>{content.title}</h2>
+            <img src={content.image.url} width={content.image.width} height={content.image.height} alt={content.title} class={cn(["object-cover","border-[#ff4f4b]", "p-2"])} />
+            {#if content.figma}
+              <a href={content.figma} class={cn(["text-teal-500", "hover:underline"])}>Figmaを見る</a>
+            {/if}
+          {/if}
+        </div>
     {/each}
   </div>
   <div class={cn(["flex", "justify-center", "items-center", "w-full"])}>
